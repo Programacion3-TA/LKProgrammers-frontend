71285c175933af8e90fd762ca5cea8448fbe4f80
ACER <ACER@LAPTOP-FN3SEJOG>
2024-06-04 17:39:07 -0500

    Se ha implementado las operaciones de admin para crear cursos

--- WebForm/View/Admin/Cursos/CursosAdmin.aspx.cs
+++ WebForm/View/Admin/Cursos/CursosAdmin.aspx.cs
@@ -21,9 +21,17 @@ namespace WebForm.View.Admin.Cursos
 
         private void cargarTabla()
         {
-            cursos = new BindingList<curso>(serviciodao.listarCursos().ToList());
-            GridCursos.DataSource = cursos;
+            var c = serviciodao.listarCursos();
+            if (c != null)
+            {
+                GridCursos.DataSource = new BindingList<curso>(c);
+            }
+            else
+            {
+                GridCursos.DataSource = new BindingList<curso>();
+            }
             GridCursos.DataBind();
+
         }
 
         protected void BtnNuevo_Click(object sender, EventArgs e)
@@ -36,16 +44,49 @@ namespace WebForm.View.Admin.Cursos
 
         protected void EditRow_Click(object sender, EventArgs e)
         {
-
+            Button btn = (Button)sender;
+            int code = int.Parse(btn.CommandArgument);
+            curso profesor = serviciodao.listarCursos().ToList().FirstOrDefault(x => x.id == code);
+            TxtCode.Text = profesor.id.ToString();
+            TxtNombre.Text = profesor.nombre;
+            TxtDescripción.Text = profesor.descripcion;
+            CallJavascript("showModalFormCurso()");
         }
 
         protected void DelRow_Click(object sender, EventArgs e)
         {
-
+            Button btn = (Button)sender;
+            string code = btn.CommandArgument;
+            serviciodao.eliminar_cursos(int.Parse(code));
+            cargarTabla();
         }
 
         protected void BtnGuardar_Click(object sender, EventArgs e)
         {
+            var cur = serviciodao.listarCursos();
+            BindingList<curso> lis_cur = new BindingList<curso>();
+            curso op = new curso();
+            if (cur != null)
+            {
+                lis_cur = new BindingList<curso>(cur);
+            }
+            if (string.IsNullOrEmpty(TxtCode.Text)) // crear
+            {
+                op.id = lis_cur.Count() + 1;
+                op.nombre = TxtNombre.Text;
+                op.descripcion = TxtDescripción.Text;
+                serviciodao.insertar_cursos(op);
+                cargarTabla();
+            }
+            else //actualizar
+            {
+                op = lis_cur.ToList().Find(x => x.id == int.Parse(TxtCode.Text));
+                op.nombre = TxtNombre.Text;
+                op.descripcion = TxtDescripción.Text;
+                serviciodao.editar_cursos(op);
+                cargarTabla();
+            }
+            Response.Redirect(Request.Url.AbsoluteUri);
 
         }
 
