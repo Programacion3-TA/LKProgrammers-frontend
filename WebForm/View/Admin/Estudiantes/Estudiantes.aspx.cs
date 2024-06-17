354715a89a29908110caf52b0120d984ec88bf86
Romina <rominavaldivia01@gmail.com>
2024-06-17 15:59:49 -0500

    mensajee

--- WebForm/View/Admin/Estudiantes/Estudiantes.aspx.cs
+++ WebForm/View/Admin/Estudiantes/Estudiantes.aspx.cs
@@ -13,38 +13,38 @@ namespace WebForm.View.Admin.Estudiantes
     public partial class Estudiantes : System.Web.UI.Page
     {
         private ServicioWS.LKServicioWebClient daoservicio;
+        private BindingList<alumno> alumnos;
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            // Comprobamos que se trata de un admin
-            if (Session["Usuario"] == null)
+            daoservicio = new ServicioWS.LKServicioWebClient();
+            
+            var resultado = daoservicio.listarAlumnos();
+            if (resultado != null)
             {
-                Response.Redirect("/View/Login/Login.aspx");
+                // Existen alumnos
+                alumnos = new BindingList<alumno>(resultado.ToList());
+                CargarTabla();
+               
             }
             else
             {
-                daoservicio = new ServicioWS.LKServicioWebClient();
-                string tipo = Session["Tipo"] as string;
-                if (tipo != "Administrador") Response.Redirect("/View/Login/Login.aspx");
-                usuario usuario_actual = Session["Usuario"] as usuario;
-            }
-            if (!IsPostBack)
-            {
-                CargarTabla(0);
-            }
+                LblNoAlumnos.Visible = true;
+            }                
+            
         }
 
-        protected void CargarTabla(int pageIndex)
+        protected void CargarTabla()
         {
-            var alumnos = new BindingList<alumno>(daoservicio.listarAlumnos());
-            GridAlumnos.PageIndex = pageIndex;
             GridAlumnos.DataSource = alumnos;
+            LblNoAlumnos.Visible = false;
             GridAlumnos.DataBind();
         }
 
         protected void GridAlumnos_PageIndexChanging(object sender, GridViewPageEventArgs e)
         {
-            CargarTabla(e.NewPageIndex);
+            GridAlumnos.PageIndex = e.NewPageIndex;
+            GridAlumnos.DataBind();
         }
 
         protected void EditRow_Click(object sender, EventArgs e)
@@ -55,6 +55,7 @@ namespace WebForm.View.Admin.Estudiantes
             alumno alumn = daoservicio.listarAlumnos().ToList().Find(alu => alu.codigoAlumno == codigoBusca);
             TxtCode.Text = alumn.codigoAlumno.ToString();
             TxtDNI.Text = alumn.dni.ToString();
+            TxtDNI.Enabled = false;
             TxtNombre.Text = alumn.nombres;
             TxtTelefono.Text = alumn.telefono;
             TxtApellidoPat.Text = alumn.apellidoPaterno;
@@ -68,15 +69,24 @@ namespace WebForm.View.Admin.Estudiantes
             TxtFechaNac.Text = alumn.fechaNac.ToString("yyyy-MM-dd");
             CallJavascript("showModalFormAgregarEstudiante()");
 
-            // Implementarla edicion en bd
         }
 
         protected void DelRow_Click(object sender, EventArgs e)
         {
             LinkButton btn = (LinkButton)sender;
             int codigo = int.Parse(btn.CommandArgument);
-            daoservicio.eliminarAlumno(codigo);
-            CargarTabla(GridAlumnos.PageIndex);
+
+            // Preguntar si se quiere eliminar de verdad
+            var elementoAEliminar = alumnos.FirstOrDefault(p => p.codigoAlumno == codigo);
+            if (elementoAEliminar == null) return;
+            alumno al = elementoAEliminar as alumno;
+            Session["alumnoAEliminar"] = al;
+            LblWarning.Text = "¡Cuidado!";
+            LblMensaje.Text = "¿Está seguro de que quiere eliminar al alumno " + al.nombres + " " + al.apellidoPaterno +
+                " con código " + al.codigoAlumno + " y DNI " + al.dni + "?";
+            BtnAceptarEliminar.Visible = true;
+            CallJavascript("showModalFormWarning()");
+            
         }
 
         protected void BtnNuevo_Click(object sender, EventArgs e)
@@ -84,6 +94,7 @@ namespace WebForm.View.Admin.Estudiantes
             // Agregar alumno
             TxtCode.Text = "";
             TxtDNI.Text = "";
+            TxtDNI.Enabled = true;
             TxtNombre.Text = "";
             TxtTelefono.Text = "";
             TxtApellidoPat.Text = "";
@@ -102,43 +113,140 @@ namespace WebForm.View.Admin.Estudiantes
         {
             alumno al = new alumno();
             bool actualizaOcrea = string.IsNullOrEmpty(TxtCode.Text);
-            if (actualizaOcrea)
-            {
-                al.codigoAlumno = daoservicio.listarAlumnos().Count() + 1;
-            }
-            else
-            {
-                al = daoservicio.listarAlumnos().ToList().Find(alu => alu.codigoAlumno == Int32.Parse(TxtCode.Text));
-            }
-            al.dni = TxtDNI.Text; //1
-            al.nombres = TxtNombre.Text; //2
-            al.apellidoPaterno = TxtApellidoPat.Text; //3
-            al.apellidoMaterno = TxtApellidoMat.Text; //4
-            al.correoElectronico = TxtCorreo.Text; //5
-            al.telefono = TxtTelefono.Text; //6
-            al.direccion = TxtDireccion.Text; //7
-            al.genero = DDGenero.SelectedValue.ToString()[0]; //8
+            
+            bool campoVacio = false;
+
+            if (!string.IsNullOrEmpty(TxtDNI.Text))
+                al.dni = TxtDNI.Text; //1            
+            else campoVacio = true;
+            if (!string.IsNullOrEmpty(TxtNombre.Text))
+                al.nombres = TxtNombre.Text; //2
+            else campoVacio = true;
+            if (!string.IsNullOrEmpty(TxtApellidoPat.Text))
+                al.apellidoPaterno = TxtApellidoPat.Text; //3
+            else campoVacio = true;
+            if (!string.IsNullOrEmpty(TxtApellidoMat.Text))
+                al.apellidoMaterno = TxtApellidoMat.Text; //4
+            else campoVacio = true;
+            if (!string.IsNullOrEmpty(TxtCorreo.Text))
+                al.correoElectronico = TxtCorreo.Text; //5
+            else campoVacio = true;
+            if (!string.IsNullOrEmpty(TxtTelefono.Text))
+                al.telefono = TxtTelefono.Text; //6
+            else campoVacio = true;
+            if (!string.IsNullOrEmpty(TxtDireccion.Text))
+                al.direccion = TxtDireccion.Text; //7
+            else campoVacio = true; 
+            if (!string.IsNullOrEmpty(DDGenero.SelectedValue))
+                al.genero = DDGenero.SelectedValue.ToString()[0]; //8
+            
             al.gradoSpecified = true;
-            al.fechaNacSpecified = true;
-            al.fechaNac = DateTime.Parse(TxtFechaNac.Text); //9
-            al.grado = (grado)Enum.Parse(typeof(grado), SLGrado.Text); //10
-            al.usuario1 = TxtUsuario.Text;
-            al.contrasenia = TxtContrasenia.Text;
+
+            if (!string.IsNullOrEmpty(TxtFechaNac.Text))
+            {
+                al.fechaNacSpecified = true;
+                al.fechaNac = DateTime.Parse(TxtFechaNac.Text); //9
+            }
+            else campoVacio = true;
+
+            if (!string.IsNullOrEmpty(SLGrado.Text))
+                al.grado = (grado)Enum.Parse(typeof(grado), SLGrado.Text); //10
+            else campoVacio = true;
+            
+            if (!string.IsNullOrEmpty(TxtUsuario.Text))
+                al.usuario1 = TxtUsuario.Text;
+            else campoVacio = true;
+
+            if (!string.IsNullOrEmpty(TxtContrasenia.Text))
+                al.contrasenia = TxtContrasenia.Text;
+            else campoVacio = true;
+
             if (actualizaOcrea)
             {
-                daoservicio.insertarAlumno(al);
+                if (campoVacio)
+                {
+                    LblWarning.Text = "¡Atención!";
+                    LblMensaje.Text = "Se deben ingresar todos los datos del administrador.";
+                    BtnAceptarEliminar.Visible = false;
+
+                    CallJavascript("showModalFormWarning()");
+                }
+                else
+                {
+                    int result = daoservicio.insertarAlumno(al);
+
+                    if (result != 0)
+                    {
+                        // se logró insertar
+                        var resultado = daoservicio.listarAlumnos();
+                        if (resultado != null)
+                        {
+                            alumnos = new BindingList<alumno>(resultado.ToList());
+                            CargarTabla();
+                        }
+                        else
+                        {
+                            // no hay alumnos
+                            LblNoAlumnos.Visible = true;
+                        }
+                        
+                    }
+                    else
+                    {
+                        // No se logró insertar. Debe deberse al UNIQUE constraint
+                        LblWarning.Text = "¡Atención!";
+                        LblMensaje.Text = "No se puede crear un alumno cuyo DNI o correo electrónico ya haya sido registrado o cuyo nombre de usuario de intranet ya exista.";
+                        BtnAceptarEliminar.Visible = false;
+
+                        CallJavascript("showModalFormWarning()");
+                    }
+                }
+   
             }
             else
             {
-                daoservicio.editarAlumno(al);
+                int result = daoservicio.editarAlumno(al);
+                if (result != 0)
+                {
+                    var resultado = daoservicio.listarAlumnos();
+                    if (resultado != null)
+                    {
+                        alumnos = new BindingList<alumno>(resultado.ToList());
+                        CargarTabla();
+                    }
+                    else LblNoAlumnos.Visible = true;
+                }
             }
-            CargarTabla(GridAlumnos.PageIndex);
+            
         }
 
         private void CallJavascript(string function)
         {
             string script = "window.onload = function() {" + function + "; };";
             ClientScript.RegisterStartupScript(GetType(), "", script, true);
+        }
+
+        protected void BtnAceptarEliminar_Click(object sender, EventArgs e)
+        {
+            alumno alumnoAEliminar = Session["alumnoAEliminar"] as alumno;
+            int resultado1 = daoservicio.eliminarAlumno(alumnoAEliminar.codigoAlumno);
+
+            var resultado2 = daoservicio.listarAlumnos();
+            if (resultado2 != null && resultado1!=0)
+            {
+                alumnos = new BindingList<alumno>(resultado2.ToList());
+                CargarTabla();
+            }
+
+        }
+
+        protected void BtnRestaurar_Click(object sender, EventArgs e)
+        {
+            LblWarning.Text = "¡Atención!";
+            LblMensaje.Text = "No se puede crear un alumno cuyo DNI o correo electrónico ya haya sido registrado o cuyo nombre de usuario de intranet ya exista.";
+            BtnAceptarEliminar.Visible = false;
+
+            CallJavascript("showModalFormWarning()");
         }
     }
 }
